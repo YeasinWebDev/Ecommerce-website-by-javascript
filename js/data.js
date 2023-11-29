f5901ceabae5ed48a857a17752ce281ff30db06f
YeasinWebDev <ar9485727@gmail.com>
2023-11-29 23:24:12 +0600

    all done

--- js/data.js
+++ js/data.js
@@ -2,84 +2,84 @@ const MenProductArray = [
     {
         id: "1",
         name: "Men Shirts",
-        img:"./products.img/t-1.jpg",
+        img:"../products.img/t-1.jpg",
         brand:'Men',
         price:"100$"
 
     }, {
         id: "2",
         name: "Men Shirts",
-        img:"./products.img/t-2.jpg",
+        img:"../products.img/t-2.jpg",
         brand:'Men',
         price:"110$"
 
     }, {
         id: "3",
         name: "Men Shirts",
-        img:"./products.img/t-3.jpg",
+        img:"../products.img/t-3.jpg",
         brand:'Men',
         price:"80$"
 
     }, {
         id: "4",
         name: "Men Shirts",
-        img:"./products.img/t-4.jpg",
+        img:"../products.img/t-4.jpg",
         brand:'Men',
         price:"90$"
 
     }, {
         id: "5",
         name: "Men Shirts",
-        img:"./products.img/t-5.jpg",
+        img:"../products.img/t-5.jpg",
         brand:'Men',
         price:"105$"
 
     }, {
         id: "6",
         name: "Men Shirts",
-        img:"./products.img/t-6.jpg",
+        img:"../products.img/t-6.jpg",
         brand:'Men',
         price:"70$"
 
     }, {
         id: "7",
         name: "Men Shirts",
-        img:"./products.img/t-7.jpg",
+        img:"../products.img/t-7.jpg",
         brand:'Men',
         price:"95$"
 
     }, {
         id: "8",
         name: "Men Shirts",
-        img:"./products.img/t-8.jpg",
+        img:"../products.img/t-8.jpg",
         brand:'Men',
         price:"99$"
 
     }, {
         id: "W1",
         name: "Women Shirts",
-        img:"./products.img/l-t-1.jpg",
+        img:"../products.img/l-t-1.jpg",
         brand:'Women',
         price:"130$"
 
     },{
         id: "W2",
         name: "Women Shirts",
-        img:"./products.img/l-t-4.jpg",
+        img:"../products.img/l-t-4.jpg",
         brand:'Women',
         price:"130$"
 
     },{
         id: "W3",
         name: "Women Shirts",
-        img:"./products.img/l-t-5.jpg",
+        img:"../products.img/l-t-5.jpg",
         brand:'Women',
         price:"130$"
 
     },{
         id: "W4",
         name: "Women Shirts",
-        img:"./products.img/l-t-6.jpg",
+        img:"../products.img/l-t-6.jpg",
         brand:'Women',
         price:"130$"
 
